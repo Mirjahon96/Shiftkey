b4d55d052156d595fc9c261604d364b7e3dab301
Auden.05@mail.ru <>
2019-11-10 14:42:22 +0500

    few changes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
 
   $(".dropdown-trigger").dropdown();
 
+  $('.tabs').tabs();
+
 })
 
 // Sidenav
@@ -12,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+// Scrollspy
 document.addEventListener('DOMContentLoaded', function () {
   var elems = document.querySelectorAll('.scrollspy');
   var instances = M.ScrollSpy.init(elems);
@@ -32,3 +35,4 @@ window.addEventListener('scroll', function (e) {
 
   }
 });
+
